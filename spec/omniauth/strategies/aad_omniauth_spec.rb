1284e4064e7bddbb09dcb500d11458f2e8c6911d
Reegan Viljoen <reegan.viljoen@platform45.com>
2023-03-17 17:31:19 +0200

    replaced tests with rspec

---
+++ spec/omniauth/strategies/aad_omniauth_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'json'
+require 'aad-omniauth'
+require 'stringio'
+
+#write test for aad omniauth
+describe OmniAuth::Strategies::AADOmniauth do
+  
+end
